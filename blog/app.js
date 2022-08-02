869dd96cf5ff0171c4915135040bdd00824f0058
ksqrt <ksqrt98@gmail.com>
2022-08-02 13:55:38 +0900

    add data

--- blog/app.js
+++ blog/app.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const cors = require("cors");
+// cors미들웨어 사용
+app.use(
+  cors({
+    origin: "*", // 모든 출처 허용 옵션. true 를 써도 된다.
+  })
+);
 
 // 라우터 정의
 const index = require("./router/index");
@@ -10,13 +16,6 @@ const topic = require("./router/topic");
 app.use("/topic", topic);
 // 정적 디랙토리 정의
 app.use(express.static(path.join(__dirname, "react-project/build")));
-
-// cors미들웨어 사용
-app.use(
-  cors({
-    origin: "*", // 모든 출처 허용 옵션. true 를 써도 된다.
-  })
-);
 
 // 서버 가동
 var server = app.listen(3001, function () {
--- blog/react-project/src/Hello.js
+++ blog/react-project/src/Hello.js
@@ -1,20 +1,29 @@
 // import { response } from "express";
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+// import { use } from "../../router";
 
 export default function Hello() {
+  const [user, setUser] = useState(null);
   async function getData(url) {
     axios
       .get(url)
       .then(function (response) {
         console.log("성공");
-        console.log(response.data[0]);
+        // console.log(response.data[0]);
+        setUser(response.data[0]);
       })
       .catch(function (error) {
         console.log("실패");
       });
   }
-  var a = getData("http://111.111.1.247:3001/serverSide/1");
+  getData("http://111.111.1.247:3001/serverSide/2");
+  // console.log(user);
 
-  return <div> {a} </div>;
+  return (
+    <div>
+      <h1>Users</h1>
+      <h1>user : {user["id"]}</h1>
+    </div>
+  );
 }
