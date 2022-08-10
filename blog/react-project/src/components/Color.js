62d1e88eda7c3a8e68185dd10d97604f188db461
ksqrt <ksqrt98@gmail.com>
2022-08-10 15:00:07 +0900

    add csr

--- blog/react-project/src/components/Color.js
+++ blog/react-project/src/components/Color.js
@@ -1,36 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 export default function Color(props) {
-  useEffect(() => {
-    return () => {
-      console.log("함수사용");
-    };
-  }, []);
-  // let 은 변수임 선언후 값 바꾸기 가능
-  const array = [1, 2, 3, 4, 5];
-  array.push(222);
-
-  for (var i = 0; i < array.length; i++) {
-    if (i === 0) continue;
-    if (array[i] === 222) break;
-    console.log(array[i]);
-  }
-
-  const superheroes = ["아이언맨", "캡틴 아메리카", "토르", "닥터 스트레인지"];
-
-  const a = superheroes.map((i) => <div>안녕하세요 {i} 입니다</div>);
-
-  console.log(superheroes);
-
+  process();
   return (
-    <div style={{ color: props.color }}>
-      안녕하세요 {props.name}
-      <div>{a}</div>
+    <div>
+      <br></br>
     </div>
   );
 }
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-Color.defaultProps = {
-  name: "이름없음",
-  color: "gray",
-};
+async function process() {
+  console.log("안녕하세요!");
+  await sleep(1000); // 1초쉬고
+  console.log("반갑습니다!");
+  await sleep(1000); // 1초쉬고
+  console.log("소통해요");
+}
+
+process().then(() => {
+  console.log("작업이 끝났어요!");
+});
